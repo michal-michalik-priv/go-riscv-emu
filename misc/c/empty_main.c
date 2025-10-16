44a98c9697fcd3925fafa8dad08bfdb4f5b1e7e5
Michał Michalik <michal.michalik.priv@gmail.com>
2025-10-16 18:41:50 +0200

    Add the basic C programs crosscompiled for RV32I
    The most basic thing to run is the empty function and some simple
    memory write to MMIO region responsible for terminal writes.

---
+++ misc/c/empty_main.c
@@ -0,0 +1,7 @@
+// A minimal C program with a stupidly empty main function. Compile with:
+//   riscv64-unknown-elf-gcc empty_main.c -o empty_main.o -march=rv32i -mabi=ilp32 -nostdlib -ffreestanding -Wall -Wextra -Werror -O2
+// After compilation you can check the output with:
+//   riscv64-unknown-elf-objdump -d -M no-aliases empty_main.o
+int main() {
+    return 0;
+}
---
+++ misc/c/terminal_mmio_write.c
@@ -0,0 +1,9 @@
+// A minimal C program with a simple memory write in the main function. Compile with:
+//   riscv64-unknown-elf-gcc terminal_mmio_write.c -o terminal_mmio_write.o -march=rv32i -mabi=ilp32 -nostdlib -ffreestanding -Wall -Wextra -Werror -O2
+// After compilation you can check the output with:
+//   riscv64-unknown-elf-objdump -d -M no-aliases terminal_mmio_write.o
+int main() {
+    char *p = (char *)0x1000;
+    *p = 'A';
+    return 0;
+}
