7b6f16864d14d5254aaec84f75c5161331eb851e
Michał Michalik <michal.michalik.priv@gmail.com>
2025-10-18 00:53:41 +0200

    Add linker script to move RAM base to 0x80000000

--- misc/c/empty_main.c
+++ misc/c/empty_main.c
@@ -1,5 +1,5 @@
 // A minimal C program with a stupidly empty main function. Compile with:
-//   riscv64-unknown-elf-gcc empty_main.c -o empty_main.o -march=rv32i -mabi=ilp32 -nostdlib -ffreestanding -Wall -Wextra -Werror -O2
+//   riscv64-unknown-elf-gcc empty_main.c -o empty_main.o -march=rv32i -mabi=ilp32 -nostdlib -ffreestanding -Wl,-T,linker.ld -Wall -Wextra -Werror -O2
 // After compilation you can check the output with:
 //   riscv64-unknown-elf-objdump -d -M no-aliases empty_main.o
 int main() {
--- misc/c/terminal_mmio_write.c
+++ misc/c/terminal_mmio_write.c
@@ -1,5 +1,5 @@
 // A minimal C program with a simple memory write in the main function. Compile with:
-//   riscv64-unknown-elf-gcc terminal_mmio_write.c -o terminal_mmio_write.o -march=rv32i -mabi=ilp32 -nostdlib -ffreestanding -Wall -Wextra -Werror -O2
+//   riscv64-unknown-elf-gcc terminal_mmio_write.c -o terminal_mmio_write.o -march=rv32i -mabi=ilp32 -nostdlib -ffreestanding -Wl,-T,linker.ld -Wall -Wextra -Werror -O2
 // After compilation you can check the output with:
 //   riscv64-unknown-elf-objdump -d -M no-aliases terminal_mmio_write.o
 int main() {
