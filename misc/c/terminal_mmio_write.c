0739eac0b2ae8470368b5ef72346a7cc5cf9efa8
Michał Michalik <michal.michalik.priv@gmail.com>
2025-10-18 23:59:02 +0200

    Change the C dummy TTY example to "Hello, World!"
    Make the example C program (and compiled ELF) a bit more interesting
    by printing "Hello, World!" instead of "A".

--- misc/c/terminal_mmio_write.c
+++ misc/c/terminal_mmio_write.c
@@ -2,8 +2,13 @@
 //   riscv64-unknown-elf-gcc terminal_mmio_write.c -o terminal_mmio_write.o -march=rv32i -mabi=ilp32 -nostdlib -ffreestanding -Wl,-T,linker.ld -Wall -Wextra -Werror -O2
 // After compilation you can check the output with:
 //   riscv64-unknown-elf-objdump -d -M no-aliases terminal_mmio_write.o
+#define DUMMY_TTY_MMIO 0x10000000
+
 int main() {
-    char *p = (char *)0x10000000;
-    *p = 'A';
-    while(1);
+    char *tty = (char *)DUMMY_TTY_MMIO;
+    const char *msg = "Hello, World!";
+    for (const char *p = msg; *p != '\0'; p++) {
+        *tty = *p;
+    }
+    while(1) {};
 }
