d01f68bac71b8a947e2d29d47c818a8294d50a0b
Michał Michalik <michal.michalik.priv@gmail.com>
2025-10-18 12:42:36 +0200

    Fix not necessary exports in the cpu/isa.go code
    It turned out I did a rookie mistake not understanding the difference
    between export/non-export variable names, so I exported unnecesary
    variables. Clean it up.

--- pkg/cpu/isa.go
+++ pkg/cpu/isa.go
@@ -7,50 +7,50 @@ import (
 	utils "github.com/Keisim/go-riscv-emu/pkg/utils"
 )
 
-// RV32I Instruction Opcodes
+// RV32I Instruction opcodes
 const (
-	OpcodeAddi = 0b0010011
-	OpcodeJalr = 0b1100111
+	opcodeAddi = 0b0010011
+	opcodeJalr = 0b1100111
 )
 
 // RV32I Funct3 for I-type instructions
 const (
-	ITypeFunc3Addi = 0b000
-	ITypeFunc3Jalr = 0b000
+	iTypeFunc3Addi = 0b000
+	iTypeFunc3Jalr = 0b000
 )
 
-// ITypeInstruction represents a parsed I-type instruction
-type ITypeInstruction struct {
+// iTypeInstruction represents a parsed I-type instruction
+type iTypeInstruction struct {
 	rd  uint32 // Destination register
 	rs1 uint32 // Source register 1
 	imm int32  // Immediate value
 }
 
-// ParseIType parses a 32-bit I-type instruction and returns an
-// ITypeInstruction struct.
-func ParseIType(instruction uint32) ITypeInstruction {
+// parseIType parses a 32-bit I-type instruction and returns an
+// iTypeInstruction struct.
+func parseIType(instruction uint32) iTypeInstruction {
 	rd := utils.BitsSlice(instruction, 7, 12)
 	rs1 := utils.BitsSlice(instruction, 15, 20)
 	imm12 := utils.BitsSlice(instruction, 20, 32)
 	imm := utils.SignExtend(imm12, 12)
 
-	return ITypeInstruction{
+	return iTypeInstruction{
 		rd:  rd,
 		rs1: rs1,
 		imm: imm,
 	}
 }
 
-// Addi executes the ADDI instruction on the given core.
-func Addi(core *Core, instr ITypeInstruction) error {
+// addi executes the ADDI instruction on the given core.
+func addi(core *Core, instr iTypeInstruction) error {
 	slog.Debug(fmt.Sprintf("Executing ADDI instruction: %+v\n", instr))
 	core.x[instr.rd] = core.x[instr.rs1] + uint32(instr.imm)
 	core.pc += 4
 	return nil
 }
 
-// Jarl executes the JALR instruction on the given core.
-func Jarl(core *Core, instr ITypeInstruction) error {
+// jarl executes the JALR instruction on the given core.
+func jarl(core *Core, instr iTypeInstruction) error {
 	slog.Debug(fmt.Sprintf("Executing JALR instruction: %+v\n", instr))
 	targetAddress := (core.x[instr.rs1] + uint32(instr.imm)) &^ 1
 	core.x[instr.rd] = core.pc + 4
@@ -60,15 +60,15 @@ func Jarl(core *Core, instr ITypeInstruction) error {
 
 // Parse parses a 32-bit instruction word and returns the corresponding
 // instruction struct based on the opcode and funct3 fields.
-func Execute(core *Core, instruction uint32) error {
+func execute(core *Core, instruction uint32) error {
 	opcode := utils.BitsSlice(instruction, 0, 7)
 	func3 := utils.BitsSlice(instruction, 12, 15)
 
 	switch {
-	case opcode == OpcodeAddi && func3 == ITypeFunc3Addi:
-		return Addi(core, ParseIType(instruction))
-	case opcode == OpcodeJalr && func3 == ITypeFunc3Jalr:
-		return Jarl(core, ParseIType(instruction))
+	case opcode == opcodeAddi && func3 == iTypeFunc3Addi:
+		return addi(core, parseIType(instruction))
+	case opcode == opcodeJalr && func3 == iTypeFunc3Jalr:
+		return jarl(core, parseIType(instruction))
 
 	default:
 		return fmt.Errorf("unsupported instruction, %032b", instruction)
@@ -78,7 +78,7 @@ func Execute(core *Core, instruction uint32) error {
 // Step fetches and executes the next instruction for the given core.
 func Step(core *Core) error {
 	instruction := core.Fetch()
-	err := Execute(core, instruction)
+	err := execute(core, instruction)
 	if err != nil {
 		return err
 	}
